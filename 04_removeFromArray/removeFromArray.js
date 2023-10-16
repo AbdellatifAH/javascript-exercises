c36c4dab91604c448b28a603011d45063bb96bb9
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-16 19:26:28 +0300

    Solved 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(a,...b) {
+    for(let i=0;i<a.length;i++)
+        for(let j=0;j<b.length;j++)
+            if(a[i]===b[j]){
+                a.splice(i,1);
+                i--;
+            }
+    return a;
 };
 
 // Do not edit below this line
