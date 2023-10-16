c9381480892a500d47601dbe8c8b6c7c55f89490
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-16 17:42:13 +0300

    Solved 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if(num<0) return "ERROR"
+    let sum='';
+for(let i=0;i<num;i++)
+{
+sum=sum+string;
+}
+return sum;
 };
 
 // Do not edit below this line
