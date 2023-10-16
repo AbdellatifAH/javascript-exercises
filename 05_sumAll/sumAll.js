2c9ca6e7e6729135248ea2ec34009206804ae359
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-16 20:24:24 +0300

    Solved 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+let sum=0;
+if(a<0 || b<0 )
+return "ERROR"
+else
+if(typeof(a)!=='number' || typeof(b)!=='number')
+return "ERROR"
+else
+if(a<b)
+    for(i=a;i<=b;i++)
+        sumall=sumall+i;
+else
+    for(i=b;i<=a;i++)
+    sumall=sumall+i;
 
+    return(sum)
 };
 
 // Do not edit below this line
