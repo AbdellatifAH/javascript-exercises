9ddd5361845766b017ffc807ef16f4059fb69fab
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-20 16:56:37 +0300

    Solved 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+    a=a.toLowerCase();
+    a=a.replace(/[.,\/#!$%? \^&\*;:{}=\-_`~()]/g,"")
+    for(let i=0;i<a.length-1;i++){
+        if(a[i]!==a[a.length-i-1])
+        return false;
+}
+return true;
 };
 
 // Do not edit below this line
