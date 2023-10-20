dc2299ef2174190e426022c0958eef36bd6ab5f0
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-20 17:05:16 +0300

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a < 0) return "OOPS";
+    if (a === 0) return 0;
 
+    let b = 1;
+    let c = 0;
+    
+    for (let i = 2; i <= a; i++) {
+        let d = b + c;
+        c = b;
+        b = d;
+    }
+
+    return b;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
