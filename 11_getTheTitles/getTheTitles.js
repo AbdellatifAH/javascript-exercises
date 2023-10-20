4fc34dc0ca2592e0da614428395d9398d83760d3
Abdellatif <147389982+AbdellatifAH@users.noreply.github.com>
2023-10-20 17:29:10 +0300

    Solved 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const booksTitles=[];
+    for(i=0;i<books.length;i++)
+    booksTitles.push(books[i].title)
+return booksTitles;
 };
 
 // Do not edit below this line
